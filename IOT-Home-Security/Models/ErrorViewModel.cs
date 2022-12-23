1fbd775abe5b53d61ae1076007116f76ed92ab2b
kddinev18 <kddinev18@codingburgas.bg>
2022-12-23 16:21:14 +0200

    Add project files.

---
+++ IOT-Home-Security/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IOT_Home_Security.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
