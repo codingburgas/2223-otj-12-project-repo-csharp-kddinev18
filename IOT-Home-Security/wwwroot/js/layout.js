76f939009a8a89f96f30934de4c11d6e27ccdb68
kddinev18 <kddinev18@codingburgas.bg>
2022-12-24 23:11:41 +0200

    Finish the drop down structure

--- IOT-Home-Security/wwwroot/js/layout.js
+++ IOT-Home-Security/wwwroot/js/layout.js
@@ -9,48 +9,103 @@
             bulets: ["Car Engine", "Milka", "Notebooks", "Kalai"],
         },
     ],
-    imagePath: `"../wwwroot/images/img.png"`,
+};
+
+let aboutContent = {
+    content: [
+        {
+            title: "AboutTitle",
+            bulets: ["Car Engine", "Milka", "Notebooks", "Kalai"],
+        },
+        {
+            title: "AboutTitle2",
+            bulets: ["Car Engine", "Milka", "Notebooks", "Kalai"],
+        },
+    ],
+};
+
+let portContent = {
+    content: [
+        {
+            title: "PortTitle",
+            bulets: ["Car Engine", "Milka", "Notebooks", "Kalai"],
+        },
+        {
+            title: "PortTitle2",
+            bulets: ["Car Engine", "Milka", "Notebooks", "Kalai"],
+        },
+    ],
+};
+
+let testContent = {
+    content: [
+        {
+            title: "TestTitle",
+            bulets: ["Car Engine", "Milka", "Notebooks", "Kalai"],
+        },
+        {
+            title: "TestTitle2",
+            bulets: ["Car Engine", "Milka", "Notebooks", "Kalai"],
+        },
+    ],
 };
 
 let extendedMenu = document.querySelector(".exteneded-menu");
 function homeButtonHover() {
     extendedMenu.style.display = "flex";
 
-    let image = document.createElement("img");
-    image.setAttribute("src", homeContent.imgPath);
-    image.classList.add("image");
+    homeContent.content.forEach(displayExtendedMenu);
+}
 
-    homeContent.content.forEach(function (item) {
-        let linkRows = document.createElement("div");
-        linkRows.classList.add("link-rows");
+function aboutButtonHover() {
+    extendedMenu.style.display = "flex";
 
-        let title = document.createElement("h3");
-        title.classList.add("title");
-        title.textContent = item.title;
+    aboutContent.content.forEach(displayExtendedMenu);
+}
 
-        let line = document.createElement("hr");
-        line.classList.add("line");
+function portButtonHover() {
+    extendedMenu.style.display = "flex";
 
-        let unorderedList = document.createElement("ul");
+    portContent.content.forEach(displayExtendedMenu);
+}
 
-        item.bulets.forEach(function (item) {
-            let listElement = document.createElement("li");
-            listElement.textContent = item;
+function testButtonHover() {
+    extendedMenu.style.display = "flex";
 
-            unorderedList.appendChild(listElement);
-        });
+    testContent.content.forEach(displayExtendedMenu);
+}
 
-        linkRows.appendChild(title);
-        linkRows.appendChild(line);
-        linkRows.appendChild(unorderedList);
+function displayExtendedMenu (item) {
+    let linkRows = document.createElement("div");
+    linkRows.classList.add("link-rows");
 
-        extendedMenu.appendChild(linkRows);
+    let title = document.createElement("h3");
+    title.classList.add("title");
+    title.textContent = item.title;
+
+    let line = document.createElement("hr");
+    line.classList.add("line");
+
+    let unorderedList = document.createElement("ul");
+
+    item.bulets.forEach(function (item) {
+        let listElement = document.createElement("li");
+        listElement.textContent = item;
+
+        unorderedList.appendChild(listElement);
     });
 
-    extendedMenu.appendChild(image);
+    linkRows.appendChild(title);
+    linkRows.appendChild(line);
+    linkRows.appendChild(unorderedList);
+
+    extendedMenu.prepend(linkRows);
 }
 
 function removeDropdown() {
-    extendedMenu.replaceChildren();
+    while (extendedMenu.childNodes.length > 2) {
+        console.log(extendedMenu.childNodes.length);
+        extendedMenu.removeChild(extendedMenu.firstChild);
+    }
     extendedMenu.style.display = "none";
 }
