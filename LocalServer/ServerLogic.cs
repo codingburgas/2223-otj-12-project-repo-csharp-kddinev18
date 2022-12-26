4c2a05a894bb9698a489b08cc0e752deb19da718
kddinev18 <kddinev18@codingburgas.bg>
2022-12-26 16:26:37 +0200

    Implement ServerSetUp() in ServerLogic class

--- LocalServer/ServerLogic.cs
+++ LocalServer/ServerLogic.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,34 @@ namespace LocalServer
 {
     public class ServerLogic
     {
+        private static TcpListener _tcpListener;
+        private static List<TcpClient> _clients = new List<TcpClient>();
+        private static Dictionary<string, TcpClient> _clientsTables = new Dictionary<string, TcpClient>();
+        // Buffer
+        private static byte[] _data = new byte[16777216];
+        private int _port;
+        private static int _success = 0;
+        private static int _error = 1;
+
+        public ServerLogic(int port)
+        {
+            _port = port;
+        }
+        public void ServerSetUp()
+        {
+            _tcpListener = new TcpListener(IPAddress.Any, _port);
+            // Starts the server
+            _tcpListener.Start();
+            // Starts accepting clients
+            _tcpListener.BeginAcceptTcpClient(new AsyncCallback(AcceptClients), null);
+        }
+
+        public void ServerShutDown()
+        {
+            // Stops the server
+            _tcpListener.Stop();
+            _tcpListener = null;
+        }
+
     }
 }
