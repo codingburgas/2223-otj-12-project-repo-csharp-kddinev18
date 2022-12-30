fcbbdcc86099eefcde4bc663f2fed5e521f2e869
kddinev18 <kddinev18@codingburgas.bg>
2022-12-30 11:55:54 +0200

    Implement InitializeDatabase() in Database class

--- DataAccessLayer/Database.cs
+++ DataAccessLayer/Database.cs
@@ -1,7 +1,22 @@
-﻿namespace DataAccessLayer
-{
-    public class Class1
-    {
+﻿using System.Data.SqlClient;
 
+namespace DataAccessLayer
+{
+    public class Database
+    {
+        public static HashSet<Table> Tables { get; set; }
+
+        private SqlConnection _sqlConnection;
+        private string _connectionString;
+        public void InitializeDatabase(string connectionString)
+        {
+            Tables = new HashSet<Table>();
+            _connectionString = connectionString;
+            if (_sqlConnection == null)
+            {
+                _sqlConnection = new SqlConnection(_connectionString);
+                _sqlConnection.Open();
+            }
+        }
     }
 }
