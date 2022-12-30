f889bcad62a14747f198bf9eb38fd1b282d64ba3
kddinev18 <kddinev18@codingburgas.bg>
2022-12-30 11:57:10 +0200

    Implement LoadDatabaseInfrastructure() in Database class

--- DataAccessLayer/Table.cs
+++ DataAccessLayer/Table.cs
@@ -1,6 +1,17 @@
-﻿namespace DataAccessLayer
+﻿using System.Data.Common;
+
+namespace DataAccessLayer
 {
     public class Table
     {
+        public string Name { get; set; }
+        public HashSet<Column> Columns { get; set; }
+
+        public Table(string name)
+        {
+            Name = name;
+            Columns = new HashSet<Column>();
+        }
+
     }
 }
