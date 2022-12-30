6107d419cbcbedfde6ef70e1d37d187ff27c94e4
kddinev18 <kddinev18@codingburgas.bg>
2022-12-30 11:58:43 +0200

    Implement LoadTableColumns() in Database class

--- DataAccessLayer/Column.cs
+++ DataAccessLayer/Column.cs
@@ -6,6 +6,16 @@
         public string Type { get; set; }
         public Table Table { get; set; }
         public HashSet<Tuple<string, object>> Constraints { get; set; }
-
+        public Column(string name, string type, Table table)
+        {
+            Constraints = new HashSet<Tuple<string, object>>();
+            Name = name;
+            Type = type;
+            Table = table;
+        }
+        public void AddConstraint(Tuple<string, object> constraint)
+        {
+            Constraints.Add(constraint);
+        }
     }
 }
