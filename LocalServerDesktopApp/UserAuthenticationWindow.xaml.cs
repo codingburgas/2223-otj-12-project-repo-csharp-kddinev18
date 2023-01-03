c10995a68ac6d163962b5b0000bdfeec5d724edb
kddinev18 <kddinev18@codingburgas.bg>
2023-01-03 10:25:48 +0200

    Add user authentication window

---
+++ LocalServerDesktopApp/UserAuthenticationWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LocalServerGUI
+{
+    /// <summary>
+    /// Interaction logic for UserAuthenticationWindow.xaml
+    /// </summary>
+    public partial class UserAuthenticationWindow : Window
+    {
+        public UserAuthenticationWindow()
+        {
+            InitializeComponent();
+        }
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            // Drag the window of the button is hold
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Remove the server connection
+            // Services.RemoveConnection();
+            // Shutdown the application
+            Application.Current.Shutdown();
+        }
+    }
+}
