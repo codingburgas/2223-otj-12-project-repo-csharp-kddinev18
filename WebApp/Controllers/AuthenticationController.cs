5dfeda2c752bc79eedf111421ea1b89c7f563478
kddinev18 <kddinev18@codingburgas.bg>
2023-02-02 13:59:15 +0200

    Add registration page

---
+++ WebApp/Controllers/AuthenticationController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(User newUser)
+        {
+            return View();
+        }
+    }
+}
