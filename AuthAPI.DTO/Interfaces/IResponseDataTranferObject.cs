5d0da20abb560e7bf6b3e154934b638f7c41598a
kddinev18 <kddinev18@codingburgas.bg>
2023-03-23 10:52:45 +0200

    Add data trasfer objects in AuthAPI

---
+++ AuthAPI.DTO/Interfaces/IResponseDataTranferObject.cs
@@ -0,0 +1,6 @@
+﻿namespace WebApp.DTO.Interfaces
+{
+    public interface IResponseDataTransferObject
+    {
+    }
+}
---
+++ AuthAPI.DTO/UserResponseDataTransferObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApp.DTO.Interfaces;
+
+namespace WebApp.DTO
+{
+    public class UserResponseDataTransferObject : IResponseDataTransferObject
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }  
+    }
+}
