abe3d106242c4d2f7a5797eade072ecdf1370245
kddinev18 <kddinev18@codingburgas.bg>
2023-03-23 10:48:26 +0200

    Add user model in AuthAPI

---
+++ AuthAPI.DAL/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WebApp.DTO;
+using WebApp.DTO.Interfaces;
+
+namespace WebApp.DAL.Models
+{
+    public class User
+    {
+        public User() { }
+        public User(IRequestDataTransferObject user) 
+        {
+            UserRequestDataTrasferObject dataTrasferObject = user as UserRequestDataTrasferObject;
+            if (dataTrasferObject.Id.HasValue)
+            {
+                Id = dataTrasferObject.Id.Value;
+            }
+            UserName = dataTrasferObject.UserName;
+            Email = dataTrasferObject.Email;
+            Password = dataTrasferObject.Password;
+            Salt = dataTrasferObject.Salt;
+        }
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Salt { get; set; }
+        public DateTime DateRegisterd { get; set; } = DateTime.Now;
+    }
+}
