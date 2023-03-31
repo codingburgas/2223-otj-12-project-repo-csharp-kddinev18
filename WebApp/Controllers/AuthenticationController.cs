951bb01a787c2c9e43482c0f06c87fdc329ecfc3
kddinev18 <kddinev18@codingburgas.bg>
2023-03-31 21:31:56 +0300

    Add validation attribute EmailAttribute

---
+++ WebApp/Controllers/AuthenticationController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult LogInLocalServer()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApp/ValidationAttributes/EmailAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace WebApp.ValidationAttributes
+{
+    public class EmailAttribute : ValidationAttribute
+    {
+        private readonly Regex _regex = new Regex(@"^[^@\s]+@[^@\s]+\.[^@\s]+$");
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                string email = value.ToString();
+                if (_regex.IsMatch(email))
+                {
+                    return ValidationResult.Success;
+                }
+                else
+                {
+                    return new ValidationResult($"The {validationContext.DisplayName} field must be a valid email address.");
+                }
+            }
+
+            return new ValidationResult($"The {validationContext.DisplayName} field must be a valid email address.");
+        }
+    }
+}
