ffea704fca52524754340f9f6e2f7e754be11150
kddinev18 <kddinev18@codingburgas.bg>
2023-03-31 21:33:30 +0300

    Add validation attribute PasswordAttribute

---
+++ WebApp/ValidationAttributes/PasswordAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.ValidationAttributes
+{
+    public class PasswordAttribute : ValidationAttribute
+    {
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                string password = value.ToString();
+                if (password.Any(char.IsUpper) &&
+                    password.Any(char.IsLower) &&
+                    password.Any(char.IsDigit) &&
+                    password.Any(c => !char.IsLetterOrDigit(c)))
+                {
+                    return ValidationResult.Success;
+                }
+                else
+                {
+                    return new ValidationResult($"The {validationContext.DisplayName} field must contain at least one uppercase letter, one lowercase letter, one digit, and one special character.");
+                }
+            }
+
+            return new ValidationResult($"The {validationContext.DisplayName} field must contain at least one uppercase letter, one lowercase letter, one digit, and one special character.");
+
+        }
+    }
+
+}
