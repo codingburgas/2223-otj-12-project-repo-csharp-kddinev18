f52800df05ccb9e1e46cda7b11d58221c4ff349a
kddinev18 <kddinev18@codingburgas.bg>
2023-04-15 19:24:28 +0300

    Add tests for the Column class

---
+++ LocalServer.TESTS/ORM/ColumnTests.cs
@@ -0,0 +1,25 @@
+﻿using LocalServer.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalServer.TESTS.ORM
+{
+    public class ColumnTests
+    {
+        [Test]
+        public void Should_ReturnSQLCode_When_InvokingToString()
+        {
+            // Arrange
+            Column column = new Column("Id", "int", new Table("Table", new Database()));
+            column.AddConstraint(new Tuple<string, object>("PRIMARY KEY", null));
+            column.AddConstraint(new Tuple<string, object>("NOT NULL", null));
+            // Act
+            string container = column.ToString();
+            // Assert
+            Assert.AreEqual(container, "[Id] int  NOT NULL ,\n");
+        }
+    }
+}
