8104d17a235f6cdb21a33830fc59fd7f799e8b49
kddinev18 <kddinev18@codingburgas.bg>
2023-04-16 14:30:31 +0300

    Add tests for the Select method in the Table class

--- LocalServer.TESTS/ORM/TableTests.cs
+++ LocalServer.TESTS/ORM/TableTests.cs
@@ -1,6 +1,7 @@
 ﻿using LocalServer.DAL;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -52,10 +53,12 @@ namespace LocalServer.TESTS.ORM
                 }
             }
 
-            table.Drop();
 
             // Assert
             Assert.That(tables.Contains(tableName));
+
+
+            table.Drop();
         }
 
         [Test]
@@ -74,7 +77,6 @@ namespace LocalServer.TESTS.ORM
 
             database.Tables.Add(table);
 
-            // Act
             table.Create();
 
             List<string> tables = new List<string>();
@@ -95,6 +97,7 @@ namespace LocalServer.TESTS.ORM
                 }
             }
 
+            // Act
             table.Drop();
 
             bool flag = false;
@@ -117,6 +120,96 @@ namespace LocalServer.TESTS.ORM
 
             // Assert
             Assert.That(tables.Contains("Persons") && !flag);
+        }
+
+        [TestCase("d68473e7-7e8a-423e-9306-97d96e3ae8e0", "Names")]
+        [TestCase("d6102451-3251-4525-9a6b-78dacda1fb3c", "Names1")]
+        [TestCase("a3dd17d3-b648-4367-90ce-f316c9c23d93", "Names123")]
+        public void Should_ReturnDataTable_When_InvokeSelectMethod(string guidString, string personName)
+        {
+            // Arrange
+            Database database = new Database(connString);
+            Table table = new Table("Persons", database);
+            Column column = new Column("Id", "nvarchar(36)", table);
+            column.AddConstraint(new Tuple<string, object>("PRIMARY KEY", null));
+            Column column2 = new Column("Name", "nvarchar(64)", table);
+            column2.AddConstraint(new Tuple<string, object>("NOT NULL", null));
+
+            table.Columns.Add(column);
+            table.Columns.Add(column2);
+
+            database.Tables.Add(table);
+
+            table.Create();
+
+            table.Insert(new Guid(guidString).ToString(), personName);
+            database.SaveDatabaseData();
+            // Act
+
+            DataTable dataTable = table.Select("Id", "=", guidString);
+
+            // Assert
+            Assert.AreEqual(dataTable.Rows[0]["Name"].ToString(), personName);
+
+            table.Drop();
+        }
+
+        [TestCase("d68473e7-7e8a-423e-9306-97d96e3ae8e0", "Names")]
+        [TestCase("d6102451-3251-4525-9a6b-78dacda1fb3c", "Names1")]
+        [TestCase("a3dd17d3-b648-4367-90ce-f316c9c23d93", "Names123")]
+        public void Should_ThrowException_When_InvokeSelectMethodWithIncorrectColumnName(string guidString, string personName)
+        {
+            // Arrange
+            Database database = new Database(connString);
+            Table table = new Table("Persons", database);
+            Column column = new Column("Id", "nvarchar(36)", table);
+            column.AddConstraint(new Tuple<string, object>("PRIMARY KEY", null));
+            Column column2 = new Column("Name", "nvarchar(64)", table);
+            column2.AddConstraint(new Tuple<string, object>("NOT NULL", null));
+
+            table.Columns.Add(column);
+            table.Columns.Add(column2);
+
+            database.Tables.Add(table);
+
+            table.Create();
+
+            table.Insert(new Guid(guidString).ToString(), personName);
+            database.SaveDatabaseData();
+
+            // Act & Assert
+            Assert.Throws<Exception>(() => table.Select("Id321", "=", guidString));
+
+            table.Drop();
+        }
+
+        [TestCase("d68473e7-7e8a-423e-9306-97d96e3ae8e0", "Names")]
+        [TestCase("d6102451-3251-4525-9a6b-78dacda1fb3c", "Names1")]
+        [TestCase("a3dd17d3-b648-4367-90ce-f316c9c23d93", "Names123")]
+        public void Should_ThrowException_When_InvokeSelectMethodWithIncorrectOperation(string guidString, string personName)
+        {
+            // Arrange
+            Database database = new Database(connString);
+            Table table = new Table("Persons", database);
+            Column column = new Column("Id", "nvarchar(36)", table);
+            column.AddConstraint(new Tuple<string, object>("PRIMARY KEY", null));
+            Column column2 = new Column("Name", "nvarchar(64)", table);
+            column2.AddConstraint(new Tuple<string, object>("NOT NULL", null));
+
+            table.Columns.Add(column);
+            table.Columns.Add(column2);
+
+            database.Tables.Add(table);
+
+            table.Create();
+
+            table.Insert(new Guid(guidString).ToString(), personName);
+            database.SaveDatabaseData();
+
+            // Act & Assert
+            Assert.Throws<Exception>(() => table.Select("Id", "asdashtf", guidString));
+
+            table.Drop();
         }
     }
 }
