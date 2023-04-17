94767e9c401ee388717e353bbde5322118d4d5b0
kddinev18 <kddinev18@codingburgas.bg>
2023-04-17 17:39:36 +0300

    Add tests for Register method in UserAuthenticationLogic

--- LocalServer.TESTS/DataManipulation/UserAuthenticationTests.cs
+++ LocalServer.TESTS/DataManipulation/UserAuthenticationTests.cs
@@ -52,5 +52,27 @@ namespace LocalServer.TESTS.DataManipulation
             Assert.NotNull(roleId);
         }
 
+        [TestCase("kddinev18", "kddinev18@abv.bg", "Password1!1!")]
+        [TestCase("TATinevich", "TATinevich@as", "sdf^%$fgDtrw543SDF")]
+        [TestCase("MilkoMilchev", "MilkoMilchev@abv.bg", "MilkoMilchev234#$")]
+        public void Should_RegisterUser_When_InvokeRegisterMethod(string userName, string email, string password)
+        {
+            // Act
+            Guid userId = UserAuthenticationLogic.Register(userName, email, password);
+
+            // Assert
+            Assert.NotNull(userId);
+        }
+
+        [TestCase("asd", "kddinev18", "Passwo")]
+        [TestCase("", "", "")]
+        [TestCase("hev", "MilkoMilchev.bg", "MilkoMilch")]
+        public void Should_ThrowException_When_InvokeRegisterMethodWithIncorrectData(string userName, string email, string password)
+        {
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => { UserAuthenticationLogic.Register(userName, email, password); });
+        }
+
+
     }
 }
